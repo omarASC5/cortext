f04a3cd3db1b93a6178d549068c349ecc73dda5d
omarASC5 <ocobas19@avenues.org>
2019-02-22 09:12:27 -0500

    console.logs all the HTML of a website entered

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-const express   = require("express"),
-		app     = express(),
-		ejs     = require("ejs"),
-		keys    = require("./config/keys.js"),
-		request = require("request"),
-		cheerio = require("cheerio");
+const express      = require("express"),
+		app        = express(),
+		ejs        = require("ejs"),
+		keys       = require("./config/keys.js"),
+		request    = require("request"),
+		cheerio    = require("cheerio"),
+		bodyParser = require("body-parser");
 
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res, next) => {
+	res.redirect("index");
+});
+
+app.get("/index", (req, res, next) => {
 	res.render("index");
 })
 
-request("https://www.nytimes.com/2019/02/20/us/north-carolina-voter-fraud.html", (err, res, html) => {
-	if (!err && res.statusCode === 200) {
-		const $ = cheerio.load(html);
-		const siteHeading = $(".css-1ygdjhk");
-		// const output = siteHeading.find("h1").text();
-		console.log(siteHeading.html());
-	} else {
-		console.log(err);
-	}
+
+
+app.post("/index", (req, res, next) => {
+	console.log(req.body.url);
+		request(req.body.url, (err, res, html) => {
+		if (!err && res.statusCode === 200) {
+			const $ = cheerio.load(html);
+			// const siteHeading = $(".css-1ygdjhk");
+			// const output = siteHeading.find("h1").text();
+			console.log($.html());
+		} else {
+			console.log(err);
+		}
+	});
+	res.send("Hi")
 });
+
 app.listen(keys.PORT, () => {
 	console.log(`Server running on port ${keys.PORT}!`);
 });
